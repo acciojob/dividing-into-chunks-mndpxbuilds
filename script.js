a6ab9f33f7dc9c4245029de5e66169887d48e559
Mandeep Sharma <mandeepvashist921@gmail.com>
2026-09-24 12:47:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 const divide = (arr, n) => {
   // Write your code here
 	let result = [];
-	for(let i=0; i<arr.size(); i++){
+	for(let i=0; i<arr.length; i++){
 		if(arr[i]==n || arr[i]<n){
 			result.push(arr[i]);
 		}else if(arr[i]+arr[i+1]<n){
