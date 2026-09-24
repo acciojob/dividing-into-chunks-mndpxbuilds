bf918e69234de12dd77a786a248ae119c0f78fd4
Mandeep Sharma <mandeepvashist921@gmail.com>
2026-09-24 12:09:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	let result = [];
+	for(let i=0; i<arr.size(); i++){
+		if(arr[i]==n || arr[i]<n){
+			result.push(arr[i]);
+		}else if(arr[i]+arr[i+1]<n){
+			result.push(arr[i],arr[i+1]);
+		}else{
+			continue;
+		}
+	}
+	console.log(result[i]);
 };
 
 const n = prompt("Enter n: ");
